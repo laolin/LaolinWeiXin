21e5d9613b8e96c987fa7f340a742c5eaee28a98
LaoLin <hi@laolin.com>
2013-05-26 23:50:12 +0800

    reply news

--- wx-app/wx-app-1.php
+++ wx-app/wx-app-1.php
@@ -1,2 +1,57 @@
 <?php
+
+$wechatLaolinObj = new wechatLaolin();
+
+class wechatLaolin {
+  
+  function __construct()
+  {
+  }
+  
+  public function About($b) {  
+    switch($b) {
+        //使用lazyRest的API，直接读wordpress的指定 页面的数据
+      case 'contact':
+        //我的Wordpress的联系页面的ID为4168
+        $this->_showSomePost('ID=4168','联系方式');
+        break;
+      
+      case 'projects':
+        $this->_showSomePost('ID=4161','工程项目');
+        break;
+      case 'awards':
+        $this->_showSomePost('ID=4163','获奖情况');
+        break;
+      case 'publications':
+        $this->_showSomePost('ID=4165','发表论文');
+        break;
+      case 'hobbies':
+        $this->_showSomePost('ID=4180','兴趣爱好');
+      
+      default:      
+        //使用lazyRest的API，直接读wordpress的指定page的全部子页面的数据
+        //我的Wordpress的简历页面的ID为4132,这个页面内容没有用
+        //所有的子页面对应简历的一个内容, 这些会由LazyREST api返回给本页面JSON数据
+        $this->_showSomePost('post_parent=4132&post_status=publish','林建萍(LaoLin) 同济大学建筑设计研究院（集团）有限公司 高级工程师 一级注册结构工程师');
+    }  
+  }
+
+  function _showSomePost($query,$title){  
+    $url='http://api.laolin.com/rest/api/wp4_posts/list/'.$query;
+
+    $rest=file_get_contents($url);
+    $res=json_decode($rest,true);
+    if($res['err_code']!=0) {
+      $data['about_laolin'][]=
+          array('post_content'=>'Error: ['.$res['err_code'].'] '.$res['err_msg']);
+    } else {
+      $data['about_laolin']=$res['data']['items'];
+      foreach ($data['about_laolin'] as $key => $row) {
+        $menu_order[$key]  = +$row['menu_order'];
+        $data['about_laolin'][$key]['post_content']=nl2br($row['post_content']);
+      }
+    }
+  }
+}
+    
 include_once ( dirname( __FILE__ ).'/../wx-inc/'.'wx-inc.php');
--- wx-inc/wx-inc.php
+++ wx-inc/wx-inc.php
@@ -28,7 +28,7 @@ class wechatCallbackapiTest
 
   public function responseMsg() {
     //get post data, May be due to the different environments
-    $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+    @$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
 
     //extract post data
     if (!empty($postStr)){
@@ -46,26 +46,84 @@ class wechatCallbackapiTest
             <MsgType><![CDATA[%s]]></MsgType>
             <Content><![CDATA[%s]]></Content>
             <FuncFlag>0</FuncFlag>
-            </xml>";             
+            </xml>"; 
+            
+      /* see: http://mp.weixin.qq.com/wiki/index.php
+      参数	描述
+      ToUserName	 接收方帐号（收到的OpenID）
+      FromUserName	 开发者微信号
+      CreateTime	 消息创建时间
+      (MsgType	 news)
+      ArticleCount	 图文消息个数，限制为10条以内
+      Articles	 多条图文消息信息，默认第一个item为大图
+      */
+      $tplNews = "
+        <xml>
+          <ToUserName><![CDATA[%s]]></ToUserName>
+          <FromUserName><![CDATA[%s]]></FromUserName>
+          <CreateTime>%s</CreateTime>
+
+          <MsgType><![CDATA[news]]></MsgType>
+
+          <ArticleCount>%d</ArticleCount>
+
+          <Articles>
+          %s
+          </Articles>
+          <FuncFlag>1</FuncFlag>
+        </xml> 
+      ";
+      
+      
+      
+      
       if($inType=='event') {
         $msgType = "text";
         $contentStr = "Thanks for your following (LaoLin-jg)!";
         $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
         echo $resultStr;
       }else if(/*$inType=='event' && */ !empty( $keyword )) {
-        $msgType = "text";
-        $contentStr = "Welcome to wechat world!";
-        $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+        //$msgType = "text";
+        //$contentStr = "Welcome to wechat world!";
+        //$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+        $news=$this->replyNews($wechatLaolinObj->About($keyword));
+        $resultStr= sprintf($tplNews, $fromUsername, $toUsername, $time, $news['n'], $news['str']);
         echo $resultStr;
       }else {
         echo "Input something...";
       }
     }else {
-        echo "";
+        echo "<img src='http://files.laolin.com/images/qrcode_for_laolin-jg.jpg'/>";
         exit;
     }
   }
     
+  private function replyNews($items) {
+      /*
+      Title	 图文消息标题
+      Description	 图文消息描述
+      PicUrl	 图片链接，支持JPG、PNG格式，较好的效果为大图640*320，小图80*80。
+      Url	 点击图文消息跳转链接
+      */
+      $tplNewsItem = "
+           <item>
+           <Title><![CDATA[%s]]></Title> 
+           <Description><![CDATA[%s]]></Description>
+           <PicUrl><![CDATA[%s]]></PicUrl>
+           <Url><![CDATA[%s]]></Url>
+           </item>
+       ";
+    $n=count($items);
+    $strItem='';
+    foreach ($items as $value) {
+      $strItem.=sprintf($tplNewsItem,$value.Title,$value.Description,$value.PicUrl,$value.Url);
+    }
+    $ret=array();
+    $ret['n']=$n;
+    $ret['str']=$strItem;
+    return $ret;
+
+  }
   private function checkSignature() {
     $signature = $_GET["signature"];
     $timestamp = $_GET["timestamp"];
