02a8cdacb21beb3f4fdda412d3ca0baf6ce7b2f5
LaoLin <hi@laolin.com>
2013-06-02 20:39:55 +0800

    TJAD工作电话查询功能

--- wx-app/wx-app-1.php
+++ wx-app/wx-app-1.php
@@ -2,17 +2,21 @@
 
 
 class wechatLaolin {
+  
   protected $_str_Welcome="欢迎关注老林的微信公众帐号。很高兴能通过这个平台与你交流。";
   protected $_str_Send0ForHelp='发送‘?’或‘0’获取帮助。';
   protected $_str_ByLaolin="\n By: LaoLin( http://laolin.com/ ).";
   
+  protected $_str_CmdUnknow='亲，未能识别的命令。';
   protected $_str_CmdChg='不好意思，亲，操作命令已改变。';
   protected $_str_SilenceIsGold="Speech is silver, silence is gold.\n\t :-)";
   
   protected $_str_Help=
-  "目前主要功能:\n【0】本帮助信息\n
-【1】老林介绍(每次结果可能不一样哟)\n
-详见 http://app.laolin.com/weixin/";
+  "目前主要功能:
+【0】本帮助信息
+【1】老林介绍(每次结果可能不一样哟)
+【TJAD】TJAD相关功能
+ http://app.laolin.com/weixin/ ";
     
   function __construct()
   {
@@ -64,9 +68,93 @@ class wechatLaolin {
         //所有的子页面对应简历的一个内容, 这些会由LazyREST api返回给本页面JSON数据
         return  $this->_showSomePost('post_parent=4132&post_status=publish');
       default: 
-        return '';
+        $ret=$this->run2($content);//复杂命令处理
+        return $ret===''?$this->_str_CmdUnknow.$this->_str_Send0ForHelp:$ret;
     }  
   }
+  
+  //复杂命令处理
+  public function run2($content) {
+  
+    if(strncasecmp('tjad',$content,4)==0){
+      return $this->_func_Tjad($content);
+    }
+  
+    return '';
+  }
+  
+  protected $_str_Tjad_Usage="感谢使用TJAD工作号码查询。
+**使用协议**
+============
+为方便大家工作设计了此号码查询功能。
+所有查询系统后台均有记录。
+请爱惜此功能，以确保此功能持续运行。
+任何的滥用行为可能导致此功能被关闭。
+请不要大量进行查询。
+（大量查询请至集团EKP平台。）
+继续查询表示你同意使用协议，否则请停止使用本功能。
+
+**功能**
+========
+查询集团职能部门、同励院、一~四院的工作电话号码。
+【TJAD+名字】查电话号码
+【TJAD+电话号码】查名字
+
+**继续查询表示你同意使用协议**";
+  protected $_str_Tjad_TelTooShort="电话号码要求至少4位。";
+  protected $_str_Tjad_Error="查询结果出错。";
+  protected $_str_Tjad_NoResult="查不到任何结果。";
+  protected $_str_Tjad_QueryDataIs="本功能仅供内部测试使用。\n你查询的是：";
+  protected $_str_Tjad_TheResultIs="查询结果：";
+  protected $_n_tjad_MaxResultCount=10;
+  protected $_str_tjad_MaxResultGot="\n最多只能返回10个结果。如有部分结果未能查到，请调整查询条件。";
+  
+  
+  
+  //复杂命令处理 之 TJAD 系列
+  protected function _func_Tjad($content) {  
+
+    $apiPath='http://api.laolin.com/rest/api/tjad_contact/list/';  
+    $kickCharList=array(" ","\t","\r","\n","\r","\v","\b",";","_",",",".","+","-","*","/",'\\',"'",'"');
+    
+    $dat=str_replace($kickCharList,'',substr($content,4));
+    if(strlen($dat)==0){
+      return $this->_str_Tjad_Usage;
+    }
+    if(is_numeric($dat)){//纯数字
+      if(strlen($dat)<4){
+        return $this->_str_Tjad_TelTooShort;
+      }
+      $apiUrl=$apiPath."tel=$dat";
+    } else {
+      $apiUrl=$apiPath."name=$dat";
+    }
+    
+    $ret=$this->getDataFromApi($apiUrl);
+    if($ret['err_code']=='0'){
+      if(! isset($ret['data']['items'])||($numberResult=count($ret['data']['items']))<=0){
+        return $this->_str_Tjad_NoResult."($dat)";
+      }
+      $txt=$this->_str_Tjad_QueryDataIs.$dat;
+      foreach ($ret['data']['items'] as $key => $row) {
+        $txt.="\n ".$row['dep'].'，'.$row['jg'].'，'.$row['name'].':'.$row['tel'];
+      }
+      if($numberResult>=$this->_n_tjad_MaxResultCount) {
+        $txt.=$this->_str_tjad_MaxResultGot;
+      }
+    } else {
+      return $this->_str_Tjad_Error.'('.$ret['err_msg'].')';
+    }
+    return $txt;
+  }
+  
+  //
+  public function getDataFromApi($url) {  
+    $rest=file_get_contents($url);
+    return json_decode($rest,true);
+  }
+  
+  
   public function welcomeStr(){
     return $this->_str_Welcome.$this->_str_Send0ForHelp.$this->_str_ByLaolin;
   }
@@ -81,7 +169,7 @@ class wechatLaolin {
       PicUrl	 图片链接，支持JPG、PNG格式，较好的效果为大图640*320，小图80*80。
       Url	 点击图文消息跳转链接
       */
-  public function _showSomePost($query,$title){  
+  public function _showSomePost($query){  
   
      // error_log( " q=$query\n", 
      // 3, dirname( __FILE__ ).'/../'.'logwx-'.TOKEN.'.log');
@@ -117,9 +205,9 @@ include_once ( dirname( __FILE__ ).'/../wx-inc/'.'wx-inc.php');
 $workerObj = new wechatLaolin();
 $wechatObj = new wechatCallbackapiTest($workerObj);
 
-if(isset( $_GET["echostr"])) {
-  $wechatObj->valid();
-} else {
+//(isset( $_GET["echostr"])) {
+//  $wechatObj->valid();
+//} else {
   $wechatObj->responseMsg();
-}
+//}
 
